6cb2a3073cbbf199f178c6f0317f407e71399ba0
BenMcPherson <thebenjaminmcpherson@gmail.com>
2024-09-28 15:12:10 -0600

    Updated Player Movement
    - Redid player assets
    - Player movement can now go in vertical
    - Improved movement code
    - Player animations added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,24 +1,39 @@
 extends CharacterBody2D
 
+#Player Variables
 const speed = 100
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
-	pass
-	
+	player_movement(delta)
+
 func player_movement(delta):
+	var direction_x = Input.get_axis("Move_Left", "Move_Right")
+	var direction_y = Input.get_axis("Move_Up", "Move_Down")
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		velocity.x = 0
-		velocity.y = speed
-	elif Input.is_action_pressed("ui_up"):
-		velocity.x = 0
-		velocity.y = -speed
+	#Animation function
+	player_animation(direction_x)
+	
+	#Horizontal Direction
+	if direction_x:
+		velocity.x = direction_x * speed
 	else:
-		velocity.x = 0
-		velocity.y = 0
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	if direction_y:
+		velocity.y = direction_y * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+	move_and_slide()
+	
+func player_animation(dir):
+	#Function variables
+	if dir > 0:
+		animated_sprite_2d.flip_h = false
+	elif dir < 0:
+		animated_sprite_2d.flip_h = true
+		
+	if dir == 0:
+		animated_sprite_2d.play("Idle")
+	else:
+		animated_sprite_2d.play("Running")
