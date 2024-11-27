dd1bf07d696b977322dabc88aa2a7fedd84d1977
BenMcPherson <thebenjaminmcpherson@gmail.com>
2024-11-26 20:36:03 -0700

    Update Shark power up

--- Scripts/scene_trigger.gd
+++ Scripts/scene_trigger.gd
@@ -6,4 +6,5 @@ var player
 func _on_body_entered(body):
 	if body is Player:
 		Global.found_companion = Dialogic.VAR.Milk_Choice
+		Global.found_power_up = Dialogic.VAR.Shark_Choice
 		scene_manager.change_scene(body.get_parent(), connected_scene)
